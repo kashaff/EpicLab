b15437331f8112b0eb45eb72481b9cfd30e31458
kashaff <58682212+kashaff@users.noreply.github.com>
2020-01-29 14:18:59 +0500

    gyrometer with esp8266 charts display

---
+++ gyro.php
@@ -0,0 +1,24 @@
+<?php
+class mpu6050{
+ public $link='';
+ function __construct($Ax, $Ay, $Az, $T, $Gx, $Gy, $Gz){
+  $this->connect();
+  $this->storeInDB($Ax, $Ay, $Az, $T, $Gx, $Gy, $Gz);
+ }
+ 
+ function connect(){
+  $this->link = mysqli_connect('localhost','root','') or die('Cannot connect to the DB');
+  mysqli_select_db($this->link,'temp') or die('Cannot select the DB');
+ }
+ 
+ function storeInDB($Ax, $Ay, $Az, $T, $Gx, $Gy, $Gz){
+  $query = "insert into mpu6050 set Ax='".$Ax."', Ay='".$Ay."', Az='".$Az."', T='".$T."', Gx='".$Gx."', Gy='".$Gy."', Gz='".$Gz."'";
+  $result = mysqli_query($this->link,$query) or die('Errant query:  '.$query);
+ }
+ 
+}
+if($_GET['Ax'] != '' or  $_GET['Ay'] != '' or $_GET['Az'] != '' or  $_GET['T'] != '' $_GET['Gx'] != '' or  $_GET['Gy'] != '' or   $_GET['Gz'] != '')
+{
+ $mpu6050=new mpu6050($_GET['Ax'],$_GET['Ay'],$_GET['Az'],$_GET['T'],$_GET['Gx'],$_GET['Gy'], $_GET['Gz']);
+}
+?>
--- k.ino
+++ k.ino
@@ -1,28 +1,169 @@
-const int trigP = 2;  //D4 Or GPIO-2 of nodemcu
-const int echoP = 0;  //D3 Or GPIO-0 of nodemcu
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <SPI.h>
 
-long duration;
-int distance;
+
+WiFiClient client;   
+
+const char* ssid = "Epic Lab";// 
+const char* password = "@@ataikram@@";
+//WiFiClient client;
+char server[] = "192.168.18.142";   //eg: 192.168.0.222
+
+
+// MPU6050 Slave Device Address
+const uint8_t MPU6050SlaveAddress = 0x68;
+
+// Select SDA and SCL pins for I2C communication 
+const uint8_t scl = D1;
+const uint8_t sda = D2;
+
+// sensitivity scale factor respective to full scale setting provided in datasheet 
+const uint16_t AccelScaleFactor = 16384;
+const uint16_t GyroScaleFactor = 131;
+
+// MPU6050 few configuration register addresses
+const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
+const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
+const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
+const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
+const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
+const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
+const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
+const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B;
+const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
+
+int16_t AccelX, AccelY, AccelZ, Temperature, GyroX, GyroY, GyroZ;
 
 void setup() {
-pinMode(trigP, OUTPUT);  // Sets the trigPin as an Output
-pinMode(echoP, INPUT);   // Sets the echoPin as an Input
-Serial.begin(9600);      // Open serial channel at 9600 baud rate
+  Serial.begin(115200);
+delay(5000);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+//  server.begin();
+  Serial.println("Server started");
+  Serial.print(WiFi.localIP());
+  delay(1000);
+  Serial.println("connecting...");
+  Wire.begin(sda, scl);
+  MPU6050_Init();
 }
 
 void loop() {
 
-digitalWrite(trigP, LOW);   // Makes trigPin low
-delayMicroseconds(2);       // 2 micro second delay 
+  double Ax, Ay, Az, T, Gx, Gy, Gz;
+  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
+  
+  //divide each with their sensitivity scale factor
+  Ax = (double)AccelX/AccelScaleFactor;
+  Ay = (double)AccelY/AccelScaleFactor;
+  Az = (double)AccelZ/AccelScaleFactor;
+  T = (double)Temperature/340+36.53; //temperature formula
+  Gx = (double)GyroX/GyroScaleFactor;
+  Gy = (double)GyroY/GyroScaleFactor;
+  Gz = (double)GyroZ/GyroScaleFactor;
 
-digitalWrite(trigP, HIGH);  // tigPin high
-delayMicroseconds(10);      // trigPin high for 10 micro seconds
-digitalWrite(trigP, LOW);   // trigPin low
+  delay(100);
 
-duration = pulseIn(echoP, HIGH);   //Read echo pin, time in microseconds
-distance= duration*0.034/2;        //Calculating actual/real distance
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    Serial.print("GET /Temp/dht.php?Ax=");
+    client.print("GET /Temp/dht.php?Ax=");     //YOUR URL
+    Serial.println(Ax);
+    client.print(Ax);
+    client.print("&Ay=");
+    Serial.println("&Ay=");
+    client.print(Ay);
+    Serial.println(Ay);
+    client.print("&Az=");
+    Serial.println("&Az=");
+    client.print(Az);
+    Serial.println(Az);
+    client.print("&T=");
+    Serial.println("&T=");
+    client.print(T);
+    Serial.println(T);
+    client.print("&Gx=");
+    Serial.println("&Gx=");
+    client.print(Gx);
+    Serial.println(Gx);
+    client.print("&Gy=");
+    Serial.println("&Gy=");
+    client.print(Gy);
+    Serial.println(Gy);
+    client.print("&Gz=");
+    Serial.println("&Gz=");
+    client.print(Gz);
+    Serial.println(Gz);
+    client.print(" ");      //SPACE BEFORE HTTP/1.1
+    client.print("HTTP/1.1");
+    client.println();
+    client.println("Host: 192.168.18.142");
+    client.println("Connection: close");
+    client.println();
+    delay(5000);
+  } else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
 
-Serial.print("Distance = ");        //Output distance on arduino serial monitor 
-Serial.println(distance);
-delay(3000);                        //Pause for 3 seconds and start measuring distance again
+
+  
+}
+
+void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+// read all 14 register
+void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.endTransmission();
+  Wire.requestFrom(deviceAddress, (uint8_t)14);
+  AccelX = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelY = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelZ = (((int16_t)Wire.read()<<8) | Wire.read());
+  Temperature = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroX = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroY = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroZ = (((int16_t)Wire.read()<<8) | Wire.read());
+}
+
+//configure MPU6050
+void MPU6050_Init(){
+  delay(150);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_CONFIG, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG, 0x00);//set +/-250 degree/second full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG, 0x00);// set +/- 2g full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_FIFO_EN, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_INT_ENABLE, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SIGNAL_PATH_RESET, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_USER_CTRL, 0x00);
 }
