b524e0d494295c549eb14482973e73c550f7c61e
kashaff <58682212+kashaff@users.noreply.github.com>
2020-01-30 19:44:44 +0500

    ESP8266+Gyrometer using AJAX and charts
    Note: copy all three files from New folder in examples folder, and dht file in Temp folder
    path to follow: http://localhost/Temp/morris.js-master/examples/refresh.html

--- dht.php
+++ dht.php
@@ -1,9 +1,10 @@
 <?php
+
 class dht11{
  public $link='';
- function __construct($temperature, $humidity){
+ function __construct($Ax, $Ay, $Az, $T, $Gx, $Gy, $Gz){
   $this->connect();
-  $this->storeInDB($temperature, $humidity);
+  $this->storeInDB($Ax, $Ay, $Az, $T, $Gx, $Gy, $Gz);
  }
  
  function connect(){
@@ -11,13 +12,14 @@ class dht11{
   mysqli_select_db($this->link,'temp') or die('Cannot select the DB');
  }
  
- function storeInDB($temperature, $humidity){
-  $query = "insert into dht11 set humidity='".$humidity."', temperature='".$temperature."'";
+ function storeInDB($Ax, $Ay, $Az, $T, $Gx, $Gy, $Gz){
+  $query = "insert into dht11 set Ax='".$Ax."', Ay='".$Ay."', Az='".$Az."', T='".$T."', Gx='".$Gx."', Gy='".$Gy."', Gz='".$Gz."'";
   $result = mysqli_query($this->link,$query) or die('Errant query:  '.$query);
  }
  
 }
-if($_GET['temperature'] != '' and  $_GET['humidity'] != ''){
- $dht11=new dht11($_GET['temperature'],$_GET['humidity']);
-}
+#if($_GET['Ax'] != '' or  $_GET['Ay'] != '' or $_GET['Az'] != '' or  $_GET['T'] != '' $_GET['Gx'] != '' or  $_GET['Gy'] != '' or   $_GET['Gz'] != ''){
+ $dht11=new dht11($_GET['Ax'],$_GET['Ay'],$_GET['Az'],$_GET['T'],$_GET['Gx'],$_GET['Gy'], $_GET['Gz']);
+#}
 ?>
+
